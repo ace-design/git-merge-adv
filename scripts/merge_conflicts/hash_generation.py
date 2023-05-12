5145d780e464bc5856200275336591723e985acd
immortalcodes <21112002mj@gmail.com>
2023-05-12 10:41:17 -0400

    csv file will have merge-base hash too

--- scripts/merge_conflicts/hash_generation.py
+++ scripts/merge_conflicts/hash_generation.py
@@ -11,10 +11,15 @@ def getHashes():
 
 def getInformation(hash):
     parent = subprocess.check_output(f"git log --pretty=%p -n 1 {hash} --abbrev=8", shell=True).decode('utf-8').strip('\n').split(' ')
+    print(parent,"\n",hash)
     left_parent=parent[0]
     right_parent=parent[1]
-
-    information={"hash":hash, "left":left_parent, "right":right_parent}
+    #there are cases when the merge base might not exist or one of the commit is actually the closest comment ancestor of other 
+    try:
+        merge_base = subprocess.check_output(f"git merge-base  {left_parent} {right_parent} ", shell=True).decode('utf-8')
+    except:
+        merge_base = ""
+    information={"hash":hash, "left":left_parent, "right":right_parent , "base":merge_base[:8]}
     return information
 
 
