55f070b4321887537576c085ded3b20e8f64baa7
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2023-05-29 10:19:00 -0400

    Removed some uncessary terminal output
    Removed print(spork.stdout) output.

--- scripts/conflict_resolving/init.py
+++ scripts/conflict_resolving/init.py
@@ -42,7 +42,6 @@ def runSpork():
         spk_res = subprocess.run(['java', '-jar', str(os.getcwd())+'/../spork-0.5.1.jar', 'left.java', 'base.java', 'right.java'],capture_output=True, text=True)
         with open("spork_result.java", "w") as res:
              res.write(spk_res.stdout)
-        print(spk_res.stdout)
 
 def runGit(repo, info,lang = None):
     suffix = 'txt'
